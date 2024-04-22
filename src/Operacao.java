18d85b57e016d756bedc3a50f27cc404f09d58b4
Rogério Oliveira <82901003+New-Masster@users.noreply.github.com>
2024-04-22 19:52:12 -0300

    Update Operacao.java

--- src/Operacao.java
+++ src/Operacao.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Operacao {
+public class Operacao {
+    private String moedaOrigem;
+    private String moedaDestino;
+    private double quantidade;
+    private double resultado;
+
+    public Operacao(String moedaOrigem, String moedaDestino, double quantidade, double resultado) {
+        this.moedaOrigem = moedaOrigem;
+        this.moedaDestino = moedaDestino;
+        this.quantidade = quantidade;
+        this.resultado = resultado;
+    }
+
+    // Getters e setters se necessário
 }
