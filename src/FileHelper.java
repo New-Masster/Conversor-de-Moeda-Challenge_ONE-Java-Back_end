f72b6a5063fcb99018f9531287ded5042cdcf0e7
Rogério Oliveira <82901003+New-Masster@users.noreply.github.com>
2024-04-22 19:51:14 -0300

    Update FileHelper.java

--- src/FileHelper.java
+++ src/FileHelper.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class FileHelper {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHelper {
+    public static String lerArquivo(String nomeArquivo) throws IOException {
+        StringBuilder conteudo = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                conteudo.append(linha).append("\n");
+            }
+        }
+        return conteudo.toString();
+    }
+
+    public static void escreverArquivo(String nomeArquivo, String conteudo) throws IOException {
+        try (FileWriter fw = new FileWriter(nomeArquivo)) {
+            fw.write(conteudo);
+        }
+    }
 }
